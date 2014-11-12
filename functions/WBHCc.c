2eab823482808ac8aabc9baaa72cdb847cc47788
aleor896 <aleor896@student.liu.se>
2014-11-12 10:27:42 +0000

    Updated indentation.

--- functions/WBHCc.c
+++ functions/WBHCc.c
@@ -12,35 +12,35 @@ static char rcs_id[] = "$Revision: 1.10 $";
 #define POLYCR (prhs[3])
 
 /* Output Arguments */
-#define	DIST   (plhs[0])
+#define  DIST   (plhs[0])
 
 void 
 mexFunction(int nlhs, mxArray  *plhs[], int nrhs, const mxArray  *prhs[])
 {
-  double *rebsimPtr;		/* pointer to theta values in radians */
-  double *polycrPtr;		/* pointer to projection coordinate array */
-  double *pr1, *pr2;	/* help pointers used in loop */
+  double *rebsimPtr;          /* pointer to theta values in radians */
+  double *polycrPtr;          /* pointer to projection coordinate array */
+  double *pr1, *pr2;          /* help pointers used in loop */
   int k, size;                /* loop counter */
-  int M, N, polysize;             /* input image size */
+  int M, N, polysize;         /* input image size */
   
 
 
   /* Check validity of arguments */
   if (nrhs != 4)
   {
-      mexErrMsgTxt("Incorrect number of input arguments");
+    mexErrMsgTxt("Incorrect number of input arguments");
   }
   if (nlhs != 1)
   {
-      mexErrMsgTxt("Incorrect number of output arguments");
+    mexErrMsgTxt("Incorrect number of output arguments");
   }
   if (mxIsSparse(M1) || mxIsSparse(N1) || mxIsSparse(REBSIM) || mxIsSparse(POLYCR))
   {
-      mexErrMsgTxt("Sparse inputs not supported");
+    mexErrMsgTxt("Sparse inputs not supported");
   }
   if (!mxIsDouble(M1) || !mxIsDouble(N1) || !mxIsDouble(REBSIM) || !mxIsDouble(POLYCR))
   {
-      mexErrMsgTxt("Inputs must be double");
+    mexErrMsgTxt("Inputs must be double");
   }
   
   pr1 = mxGetPr(M1);
@@ -71,31 +71,31 @@ mexFunction(int nlhs, mxArray  *plhs[], int nrhs, const mxArray  *prhs[])
 
 static void WBHC(double *rebsimPtr, double *polycrPtr, double *distPtr, int M, int N, int polysize)
 {
-    int i, j, k;
-    double value;
+  int i, j, k;
+  double value;
 
-    for(i=0;i<M;++i)
+  for(i=0;i<M;++i)
+  {
+    for(j=0;j<N;++j)
     {
-        for(j=0;j<N;++j)
+      value = rebsimPtr[i*N + j];
+      
+      for(k=0;k<polysize-2;++k)
+      {
+        
+        if(k == 0 && value < polycrPtr[k])
         {
-            value = rebsimPtr[i*N + j];
-            
-            for(k=0;k<polysize-2;++k)
-            {
-                
-                if(k == 0 && value < polycrPtr[k])
-                {
-                    distPtr[i*N + j] = value / polycrPtr[0];
-                    break;
-                }
-                else if(( polycrPtr[k] <= value) && (value < polycrPtr[k+1]))
-                {
-                    distPtr[i*N + j] = (k + 1) + (value - polycrPtr[k]) / (polycrPtr[k+1] - polycrPtr[k]); 
-                    break;
-                }
-            }
+          distPtr[i*N + j] = value / polycrPtr[0];
+          break;
         }
+        else if(( polycrPtr[k] <= value) && (value < polycrPtr[k+1]))
+        {
+          distPtr[i*N + j] = (k + 1) + (value - polycrPtr[k]) / (polycrPtr[k+1] - polycrPtr[k]); 
+          break;
+        }
+      }
     }
+  }
 }
 
-	
+  
